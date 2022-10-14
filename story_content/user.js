f3413bb88ad36d8009e9722d792fb11311e778cd
unknown <ashurafa@hotmail.com>
2022-10-14 23:45:37 +0300

    QQQ
    QQQ

---
+++ story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6oDJpGp84NE":
+        Script1();
+        break;
+      case "6cg8JWFmksU":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.location.reload();
+}
+
+function Script2()
+{
+  window.location.reload();
+}
+
